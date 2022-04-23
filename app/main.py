4424522353ac8034d8f8e8c8752a590c2dea33a1
adam <korba.adam@gmail.com>
2022-04-23 11:02:19 +0200

    Create Barcode functionality

--- app/main.py
+++ app/main.py
@@ -2,13 +2,17 @@ from typing import Optional
 
 from fastapi import FastAPI
 from dotenv import load_dotenv
+import requests
 import os
 
 app = FastAPI()
 
 load_dotenv()
 
-#print(os.getenv("TEST"))
+# print(os.getenv("TEST"))
+
+use_mock = 1
+
 
 @app.get("/")
 def read_root():
@@ -17,7 +21,20 @@ def read_root():
 
 @app.get("/barcode")
 def read_barcode(q: str):
-    return {"echo": q}
+    if(use_mock):
+        if(q == "97801401573122"):
+            brand = "Apple"
+        elif(q == "9780140157375"):
+            brand = "Google"
+        elif(q == "9780140157374"):
+            brand = "Microsoft"
+        else:
+            brand = "Unknown"
+    else:
+        url = f"https://api.barcodelookup.com/v3/products?barcode=9780140157376&formatted=y&key={os.getenv('BARCODE_LOOKUP_API_KEY')}"
+        r = requests.get(url=url)
+        brand = r.json()['products'][0]['brand']
+    return {"echo": brand}
 
 
 @app.get("/brand_rating")
